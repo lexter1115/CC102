7e42e92ed6f57b93ee9ce4b398048721d74322ff
lexter1115 <paquitalexter9@gmail.com>
2024-10-03 10:02:00 +0800

    Add files via upload

---
+++ lexter_01hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+
+{
+	cout << "HOWDY LOURDENIAN PROGRAMMERS!";
+	return 0;
+}
+	
+	
+
+	
---
+++ lexter_02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout<< "School\t\t\t	:	Our Lady of Lourdes College Foundation, INC." << endl;
+	cout<< "Department\t\t\t:	OLLCF College of Computer Studies" << endl;
+	cout<< "Course & Year Level\t\t:	BSIT 1-A" << endl;
+	cout<< "Full Name\t\t	:	Lexter Jhon Paquita" << endl;
+	cout<< "Address\t\t\t	:	Bakiad P-1a Labo, Camarines Norte" << endl;
+	
+return 0;
+}
